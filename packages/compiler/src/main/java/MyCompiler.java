e0f0e60b2e69235d562f27a61b36fdc57b3dff68
vikr01 <vik120@icloud.com>
2025-05-15 16:55:21 -0700

    attempt at using teavm to compile java to wasm

---
+++ packages/compiler/src/main/java/MyCompiler.java
@@ -0,0 +1,16 @@
+import com.google.template.soy.SoyFileSet;
+import com.google.template.soy.jssrc.SoyJsSrcOptions;
+import java.util.List;
+
+public class MyCompiler {
+    public static String compile() {
+        String soy = "{namespace ex}\n{template .t}\n{@param name: string}\nHello {$name}!\n{/template}";
+        SoyFileSet sfs = SoyFileSet.builder().add(soy, "inline.soy").build();
+        List<String> js = sfs.compileToJsSrc(new SoyJsSrcOptions(), null);
+        return js.get(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compile());
+    }
+}
