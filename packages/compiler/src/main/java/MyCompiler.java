7d66237a9d7b20ad2fa791e151b1e44be2d96044
vikr01 <vik120@icloud.com>
2025-05-15 23:29:30 -0700

    update compiler java

--- packages/compiler/src/main/java/MyCompiler.java
+++ packages/compiler/src/main/java/MyCompiler.java
@@ -1,16 +1,30 @@
 import com.google.template.soy.SoyFileSet;
 import com.google.template.soy.jssrc.SoyJsSrcOptions;
+
 import java.util.List;
 
 public class MyCompiler {
-    public static String compile() {
-        String soy = "{namespace ex}\n{template .t}\n{@param name: string}\nHello {$name}!\n{/template}";
-        SoyFileSet sfs = SoyFileSet.builder().add(soy, "inline.soy").build();
-        List<String> js = sfs.compileToJsSrc(new SoyJsSrcOptions(), null);
-        return js.get(0);
-    }
-
     public static void main(String[] args) {
-        System.out.println(compile());
+        String soyCode =
+            "{namespace example autoescape=\"deprecated\"}\n" +
+            "/** Says hello */\n" +
+            "{template .hello}\n" +
+            "  {@param name: string}\n" +
+            "  Hello, {$name}!\n" +
+            "{/template}";
+
+        // ✅ Avoid passing a filename to skip any file/resource I/O internally
+        SoyFileSet soyFileSet = SoyFileSet.builder()
+            .add(soyCode, /* filename */ null)
+            .build();
+
+        // ✅ Safe options setup for WASM/native-image
+        SoyJsSrcOptions jsOptions = SoyJsSrcOptions.builder().build();
+
+        List<String> jsOutput = soyFileSet.compileToJsSrc(jsOptions, null);
+
+        for (String js : jsOutput) {
+            System.out.println(js);
+        }
     }
 }
